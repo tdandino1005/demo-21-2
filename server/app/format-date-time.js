aa5bdd90c90d8b4be540c0fb0a7aeb286b3cc914
Manav Misra <manavm@visionwebsoft.com>
2023-03-13 17:20:31 -0500

    Add formatCurrentDateTime

---
+++ server/app/format-date-time.js
@@ -0,0 +1,18 @@
+function formatCurrentDateTime() {
+  const current = new Date();
+  const options = {
+    weekday: "short",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "numeric",
+    minute: "2-digit",
+    hour12: true,
+  };
+
+  return current.toLocaleDateString("en-US", options);
+}
+
+console.log(formatCurrentDateTime());
+
+export default formatCurrentDateTime;
