bb66b8bcd5b6e03b0cde542a1163fe4159ec7627
Tony Dandino <tdandino1005@gmail.com>
2023-03-14 19:59:27 -0500

    Provide apollo client

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,19 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./main.css";
 
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>
 );
