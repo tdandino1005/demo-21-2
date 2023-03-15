583af478fc5f5d161e40633cfe04e21d5956403d
Tony Dandino <tdandino1005@gmail.com>
2023-03-14 20:35:31 -0500

    Added app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,16 @@
 import { useQuery } from "@apollo/client";
-import { THOUGHTS } from "./graphql/queries";
+import { THOUGHTS } from "./graphql/type-defs";
 
 export default function App() {
   const { loading, error, data } = useQuery(THOUGHTS);
 
-  return <h1>Hello World</h1>;
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error</p>;
+
+  return data.thoughts.map(({ id, thoughtText, thoughtAuthor }) => (
+    <ul key={id}>
+      <li>{thoughtText}</li>
+      <li>{thoughtAuthor}</li>
+    </ul>
+  ));
 }
