aa6c00377a59bff95bfdd933a5a218eb36f114d6
Tony Dandino <tdandino1005@gmail.com>
2023-03-14 21:29:51 -0500

    app.jsx updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,32 @@
-import Profiles from "./routes/profiles";
+import Profiles from ".routes/Profiles";
+import Thoughts from ".routes/Thoughts";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/profiles",
+    element: <Profiles />,
+  },
+  {
+    path: "/thoughts",
+    element: <Thoughts />,
+  },
+]);
 
 export default function App() {
-  return <Profiles />;
+  return (
+    <RouterProvider router={router}>
+      <nav>
+        <ul>
+          <li>
+            <link to="/profiles">Profiles</link>
+          </li>
+          <li>
+            <link to="/thoughts">Thoughts</link>
+          </li>
+        </ul>
+      </nav>
+      ;
+    </RouterProvider>
+  );
 }
